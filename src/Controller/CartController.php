ce5966749f78c36a392e6a7d02427be97d384f45
amondp <paulamond@gmail.com>
2019-04-21 23:59:37 +0100

    Post-Deadline Bits

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -7,60 +7,58 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Login;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 
 class CartController extends AbstractController
 {
-    /**
-     * @Route("/cart", name="cart") methods={"GET","POST"}
-     */
-    public function index()
-    {
-		$request = Request::createFromGlobals(); // the envelope, and were looking inside it.
+	private $session;
 
-        $type = $request->request->get('type', 'none');
-		
-		if ($type == 'register'){
-			//perform register process
-			
-			//get variables
-			$username = $request->request->get('username', 'none');
-			$password = $request->request->get('password', 'none');
-			$acctype = $request->request->get('acctype', 'none');
-			
-			
-			//put in database
-			$entityManager = $this -> getDoctrine() -> getManager();
-			
-			$login = new Login();
-			$login->setUsername($username);
-			$login->setPassword($password);
-			$login->setAcctype($acctype);
-			
-			$entityManager->persist($login);
-			
-			$entityManager->flush();
-			
-			return new Response(
-		'Register Page was called');
-		}
-		else if ($type == 'login'){
-			
-			//get username & password
-			$username = $request->request->get('username', 'none');
-			$password = $request->request->get('password', 'none');
-			
-			//comparison of variable against DB records
-			$repo = $this->getDoctrine() -> getRepository(Login::class); // the type of the entity
-				$person = $repo->findOneBy([
-			
-				'username'=>$username,
-				'password'=>$password,
-				]);
-				
-				return new Response(
-				$person->getAcctype()//sends back account type
-				);
-			
-		}
-	}
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+	
+    /**
+     * @Route("/cartController", name="cartController") methods={"GET","POST"}
+     */
+    public function index(SessionInterface $session)
+    {
+        $request = Request::createFromGlobals(); // the envelope, and were looking inside it.
+
+        // catch the variables we sent from the JavaScript.
+        $placedby = $request->request->get('placedby', 'this is the default');
+        $orderdetails = $request->request->get('orderdetails', 'this is the default');
+        $address = $request->request->get('deliveryaddress', 'this is the default');
+      
+      // Break apart the serialized order
+      // $data = explode('=', 'cookies-2=pizza-2='); <--- this is what order details look like
+        foreach($data as $record) {    
+            $item = explode('-',$record);
+            echo 'Item: ' . $item[0] . '<br>';
+            echo 'Qty: ' . $item[1] . '<br>';
+			echo 'Subtotal: ' . $item[2] . '<br>';
+        }        
+           
+        
+        // to work the objects
+        $entityManager = $this->getDoctrine()->getManager();
+
+        // create blank entity of type "Orders"
+        $order = new orders();
+        
+        $order->setPlacedBy($placedby);
+        $order->setDetails(substr($ser, 0, -1));
+
+
+      
+        $entityManager->persist($order);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+       
+        return new Response(
+            'all ok' . $ser
+        );
+    }
 }
